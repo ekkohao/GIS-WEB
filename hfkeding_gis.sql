2a5f674678e3fc8e26f8743dc0d6e9c1d05deb41
jerehao <jerehao@gmail.com>
2016-07-06 00:31:09 +0800

    数据库类完成添加设备，分组和线路

--- hfkeding_gis.sql
+++ hfkeding_gis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 2016-07-05 09:17:04
--- 服务器版本： 5.7.9
--- PHP Version: 5.6.15
+-- Host: localhost
+-- Generation Time: 2016-07-05 16:30:20
+-- 服务器版本： 5.7.13-log
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,86 +23,178 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- 表的结构 `action_msg`
+-- 表的结构 `alarms`
 --
 
-DROP TABLE IF EXISTS `action_msg`;
-CREATE TABLE IF NOT EXISTS `action_msg` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `alarms` (
+  `id` int(10) UNSIGNED NOT NULL,
   `action_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `action_num` tinyint(3) UNSIGNED NOT NULL,
   `i_num` smallint(5) UNSIGNED NOT NULL,
   `tem` tinyint(4) NOT NULL,
   `hum` tinyint(4) NOT NULL,
   `dev_id` int(10) UNSIGNED NOT NULL,
-  `is_read` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `action_time` (`action_time`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `is_read` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `dev_list`
+-- 表的结构 `devs`
 --
 
-DROP TABLE IF EXISTS `dev_list`;
-CREATE TABLE IF NOT EXISTS `dev_list` (
-  `dev_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `devs` (
+  `dev_id` int(10) UNSIGNED NOT NULL,
   `dev_number` char(11) NOT NULL,
   `dev_name` char(30) NOT NULL,
-  `group_id` int(10) UNSIGNED NOT NULL,
-  `line_id` int(10) UNSIGNED NOT NULL,
   `dev_phase` char(4) NOT NULL,
-  PRIMARY KEY (`dev_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `group_id` int(10) UNSIGNED NOT NULL,
+  `line_id` int(10) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `devs`
+--
+
+INSERT INTO `devs` (`dev_id`, `dev_number`, `dev_name`, `dev_phase`, `group_id`, `line_id`) VALUES
+(1, '0912345678', '测试设备', 'A相', 1, 6),
+(2, '0912345688', '测试设备', 'A相', 1, 6);
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `group_list`
+-- 表的结构 `groups`
 --
 
-DROP TABLE IF EXISTS `group_list`;
-CREATE TABLE IF NOT EXISTS `group_list` (
-  `group_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `groups` (
+  `group_id` int(10) UNSIGNED NOT NULL,
   `group_name` char(30) NOT NULL,
-  `group_loc` char(50) NOT NULL,
+  `group_loc` char(30) NOT NULL,
   `line_id` int(10) UNSIGNED NOT NULL,
-  PRIMARY KEY (`group_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `line_id2` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `groups`
+--
+
+INSERT INTO `groups` (`group_id`, `group_name`, `group_loc`, `line_id`, `line_id2`) VALUES
+(1, '1号杆塔', '圣惠路', 6, NULL),
+(2, '1号杆塔', '圣惠路', 6, NULL),
+(3, '1号杆塔', '圣惠路', 6, NULL),
+(4, '1号杆塔', '圣惠路', 6, NULL),
+(5, '1号杆塔', '圣惠路', 6, NULL),
+(6, '1号杆塔', '圣惠路', 6, NULL),
+(7, '1号杆塔', '圣惠路', 6, NULL),
+(8, '1号杆塔', '圣惠路', 6, NULL),
+(9, '1号杆塔', '圣惠路', 6, NULL);
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `line_list`
+-- 表的结构 `lines`
 --
 
-DROP TABLE IF EXISTS `line_list`;
-CREATE TABLE IF NOT EXISTS `line_list` (
-  `line_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `lines` (
+  `line_id` int(10) UNSIGNED NOT NULL,
   `line_name` varchar(50) NOT NULL,
-  `add_time` timestamp NOT NULL,
-  PRIMARY KEY (`line_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `add_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `lines`
+--
+
+INSERT INTO `lines` (`line_id`, `line_name`, `add_time`) VALUES
+(1, 'aaa', '2016-07-05 15:13:32'),
+(2, 'mm', '2016-07-05 15:22:38'),
+(3, 'ccc', '2016-07-05 15:24:14'),
+(6, '高新线', '2016-07-05 15:49:34');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `user_list`
+-- 表的结构 `users`
 --
 
-DROP TABLE IF EXISTS `user_list`;
-CREATE TABLE IF NOT EXISTS `user_list` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
   `user_name` char(18) NOT NULL,
   `passwd` char(32) NOT NULL,
-  `last_login_time` timestamp NOT NULL,
-  `register_time` timestamp NOT NULL,
-  `user_role` tinyint(3) UNSIGNED NOT NULL DEFAULT '10',
-  PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `last_login_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_role` tinyint(3) UNSIGNED NOT NULL DEFAULT '10'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alarms`
+--
+ALTER TABLE `alarms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `action_time` (`action_time`),
+  ADD KEY `FK_DEV_ID` (`dev_id`);
+
+--
+-- Indexes for table `devs`
+--
+ALTER TABLE `devs`
+  ADD PRIMARY KEY (`dev_id`),
+  ADD UNIQUE KEY `dev_number` (`dev_number`),
+  ADD KEY `dev_id` (`dev_id`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`group_id`);
+
+--
+-- Indexes for table `lines`
+--
+ALTER TABLE `lines`
+  ADD PRIMARY KEY (`line_id`),
+  ADD UNIQUE KEY `line_name` (`line_name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `alarms`
+--
+ALTER TABLE `alarms`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- 使用表AUTO_INCREMENT `devs`
+--
+ALTER TABLE `devs`
+  MODIFY `dev_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- 使用表AUTO_INCREMENT `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `group_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- 使用表AUTO_INCREMENT `lines`
+--
+ALTER TABLE `lines`
+  MODIFY `line_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- 使用表AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
